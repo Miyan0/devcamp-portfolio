a01a8780dc710a5ee35fa4cafc8916a04d423290
Andre Masse <dr000w@gmail.com>
2017-10-19 08:27:02 -0400

    Customized portfolio routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,17 @@
 Rails.application.routes.draw do
-  resources :portfolios
+  
+  # custom routing here (portfolio singular for show) 
+  resources :portfolios, except: [:show]
+  # we still have to change our link_to in the index view:
+  #   change portfolio_show to portfolio_show_path
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
 
   # endpoints can be name anything
   get 'asdfasdf', to: 'pages#contact'
-  
+
   resources :blogs
   root to: 'pages#home'
 end
