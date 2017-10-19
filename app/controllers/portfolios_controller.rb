a848dbd6c9ae57963102c5f66b3aa73183df91f3
Andre Masse <dr000w@gmail.com>
2017-10-18 23:39:46 -0400

    Implemented show functionality for portfolio module

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -3,6 +3,7 @@ class PortfoliosController < ApplicationController
     @portfolio_items = Portfolio.all
   end
 
+
   def new
     @portfolio_item = Portfolio.new
   end
@@ -10,13 +11,13 @@ class PortfoliosController < ApplicationController
   def create
     @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
     
-        respond_to do |format|
-          if @portfolio_item.save
-            format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully created.' }
-          else
-            format.html { render :new }
-          end
-        end
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
   end
 
   def edit
@@ -25,7 +26,7 @@ class PortfoliosController < ApplicationController
   
   def update
     @portfolio_item = Portfolio.find(params[:id])
-    
+
     respond_to do |format|
       if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
         format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully updated.' }
@@ -34,4 +35,10 @@ class PortfoliosController < ApplicationController
       end
     end
   end
+
+
+  def show
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
 end
