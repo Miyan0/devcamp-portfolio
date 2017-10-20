678f026727ecc417c1649614570de9091e7bf60b
Andre Masse <dr000w@gmail.com>
2017-10-20 13:43:40 -0400

    Customized registration, login, and logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   
-  devise_for :users
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
   # custom routing here (portfolio singular for show) 
   resources :portfolios, except: [:show]
   # we still have to change our link_to in the index view:
