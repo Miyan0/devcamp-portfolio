39d730f78956ac8214aae7a5eb513d28c2b8eac7
Andre Masse <dr000w@gmail.com>
2017-10-20 17:39:08 -0400

    Added guest user for current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,6 @@ class ApplicationController < ActionController::Base
   # allow devise to whitelist our name field we added.
   include DeviseWhitelist # see concerns/devise_whitelist.rb
   
-
   # track where user is coming from (twitter, facebook etc)
   # this works by using a special url we send on social media that
   # when clicked includes a query parameter we're gonna track
@@ -12,5 +11,7 @@ class ApplicationController < ActionController::Base
   #     mycompany.com/about/?q=twitter
   include SetSource # see concerns/set_source
   
+  # null object pattern for current_user provided by devise
+  include CurrentUserConcern # see concerns/current_user_concern.rb
 
 end
