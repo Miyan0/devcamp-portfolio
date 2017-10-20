407d0db73f264381bf665ace47959a77d3a60880
Andre Masse <dr000w@gmail.com>
2017-10-20 14:34:27 -0400

    Added controller concerns for devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   # allow devise to whitelist our name field we added.
-  before_action :configure_permitted_parameters, if: :devise_controller?
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
-  end
+  include DeviseWhitelist # see concerns/devise_whitelist
 
 end
