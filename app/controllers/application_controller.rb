130416960a38e0c2881a22dad4e3b9899581faf8
Andre Masse <dr000w@gmail.com>
2017-10-20 15:03:47 -0400

    Completed authentication with virtual attributes for user first and last name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   # allow devise to whitelist our name field we added.
-  include DeviseWhitelist # see concerns/devise_whitelist
+  include DeviseWhitelist # see concerns/devise_whitelist.rb
 
 end
