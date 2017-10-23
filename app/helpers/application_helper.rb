9135acc24c13289edc4d8e0b0f4dd2dd5991c3f5
Andre Masse <dr000w@gmail.com>
2017-10-23 19:11:47 -0400

    Fixed bug with login helper by setting defaults.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,12 @@
 module ApplicationHelper
-  def login_helper style
-     if current_user.is_a?(GuestUser) 
-      (link_to "Register", new_user_registration_path, class: style) +
-      " ".html_safe +
-      (link_to "Login", new_user_session_path, class: style)
-     else 
-      link_to "Logout", destroy_user_session_path, method: :delete, class: style    
-     end 
+  def login_helper(style = '')
+    if current_user.is_a?(GuestUser)
+      (link_to 'Register', new_user_registration_path, class: style) +
+          ' '.html_safe +
+          (link_to 'Login', new_user_session_path, class: style)
+    else
+      link_to 'Logout', destroy_user_session_path, method: :delete, class: style
+    end
   end
 
   # to test this,
@@ -19,7 +19,7 @@ module ApplicationHelper
   def source_helper(layout_name)
     if session[:source]
       greeting = "Thanks for visiting me from #{session[:source]} and you are on the #{layout_name} layout."
-      content_tag(:p, greeting, class: "source-greeting")
+      content_tag(:p, greeting, class: 'source-greeting')
     end
   end
 
